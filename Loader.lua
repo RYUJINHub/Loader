10f475becef6a493f1e9ddac6cb3f13f32613737
RYUJINHub <142650366+RYUJINHub@users.noreply.github.com>
2024-10-07 23:19:50 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -415,7 +415,7 @@ ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
 ImageButton.BorderSizePixel = 0
 ImageButton.Position = UDim2.new(0.156000003, 0, -0, 0)
 ImageButton.Size = UDim2.new(0, 50, 0, 50)
-ImageButton.Image = "rbxassetid://75509128012159"
+ImageButton.Image = "rbxassetid://12975600393"
 ImageButton.MouseButton1Click:Connect(function()
 game.CoreGui:FindFirstChild("ScreenGui").Enabled = not game.CoreGui:FindFirstChild("ScreenGui").Enabled
 end)
